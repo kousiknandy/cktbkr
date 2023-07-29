eabdcc8f64ac11f52b3fe4c8aaeb62766b8301c8
Kousik Nandy <kousiknandy@gmail.com>
2023-07-29 02:05:58 +0100

    add serial

---
+++ serial_crawler.py
@@ -0,0 +1,18 @@
+import asyncio
+import aiohttp
+
+from circuitbreaker import Circuitbreaker
+
+@Circuitbreaker("serial")
+async def get_url(url):
+    async with aiohttp.ClientSession() as client:
+        async with client.get(url) as page:
+            assert page.status == 200
+
+async def main():
+    for i in range(32):
+        t = asyncio.create_task(get_url("http://localhost:8080/foo"))
+        await asyncio.gather(t)
+        await asyncio.sleep(1)
+
+asyncio.run(main())
